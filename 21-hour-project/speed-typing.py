7a3e89245efcf1657652984c55e701909f54b2b7
oumernoother <oumeerabdulshikur@gmail.com>
2024-10-03 07:40:17 -0400

    speed-typing

--- 21-hour-project/speed-typing.py
+++ 21-hour-project/speed-typing.py
@@ -1,9 +1,39 @@
 import curses
 from curses import wrapper
-
-def main(stdscr):
+def start_screen(stdscr):
     stdscr.clear()
-    stdscr.addstr("Hello word!")
+    stdscr.addstr("Welcome to the speed typing test!")
+    stdscr.addstr("\nPress any key to begin!")
     stdscr.refresh()
     stdscr.getkey()
+
+def wpm_test(stdscr):
+    target_text = "Hello world this is some test text for this app!"
+    current_text = []
+    # stdscr.clear()
+    # stdscr.addstr(target_text)
+    # stdscr.refresh()
+    # stdscr.getkey()
+
+    while True:
+        stdscr.clear()
+        stdscr.addstr(target_text)
+        for char in current_text:
+            stdscr.addstr(char, curses.color_pair(1))
+        stdscr.refresh()
+
+        key = stdscr.getkey()
+        if ord(key) == 27:
+            break
+        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+            if len(current_text) > 0:
+                current_text.pop()
+        else:
+            current_text.append(key)
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    start_screen(stdscr)
+    wpm_test(stdscr)
 wrapper(main)
