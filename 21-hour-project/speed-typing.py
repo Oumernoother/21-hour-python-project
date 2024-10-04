58bf3489a31224faee796b9b2c19ac97664db945
oumernoother <oumeerabdulshikur@gmail.com>
2024-10-04 03:27:53 -0400

    speed-typing

--- 21-hour-project/speed-typing.py
+++ 21-hour-project/speed-typing.py
@@ -1,4 +1,5 @@
 import curses
+import time
 from curses import wrapper
 def start_screen(stdscr):
     stdscr.clear()
@@ -7,28 +8,52 @@ def start_screen(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
+def display_text(stdscr, target, current, wpm=0):
+    # global current_text,target_text
+    stdscr.addstr(target)
+    stdscr.addstr(1,0,f"WPM: {wpm}")
+    for i, char in enumerate(current):
+        correct_char = target[i]
+        color = curses.color_pair(1)
+        if char != correct_char:
+            color = curses.color_pair(2)
+        stdscr.addstr(0,i, char, color)
+
 def wpm_test(stdscr):
     target_text = "Hello world this is some test text for this app!"
     current_text = []
+    wpm = 0
+    start_time = time.time()
+    stdscr.nodelay(True)
     # stdscr.clear()
     # stdscr.addstr(target_text)
     # stdscr.refresh()
     # stdscr.getkey()
 
     while True:
+        time_elapsed = max(time.time() - start_time, 1)
+        wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
         stdscr.clear()
-        stdscr.addstr(target_text)
-        for char in current_text:
-            stdscr.addstr(char, curses.color_pair(1))
+        display_text(stdscr, target_text, current_text, wpm)
+        # stdscr.addstr(target_text)
+        # for char in current_text:
+            # stdscr.addstr(char, curses.color_pair(1))
         stdscr.refresh()
 
-        key = stdscr.getkey()
-        if ord(key) == 27:
+        if "".join(current_text) == target_text:
+            stdscr.nodelay(False)
             break
+
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
         if key in ("KEY_BACKSPACE", '\b', "\x7f"):
             if len(current_text) > 0:
                 current_text.pop()
-        else:
+        # if ord(key) == 27:
+        #     break
+        elif len(current_text) < len(target_text):
             current_text.append(key)
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
@@ -36,4 +61,7 @@ def main(stdscr):
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
     start_screen(stdscr)
     wpm_test(stdscr)
+
+    stdscr.addstr(2,0, "You completed the text! Press any key to play again")
+    stdscr.getkey()
 wrapper(main)
